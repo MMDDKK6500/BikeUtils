31fdf1db25c6ad666d6c196015db626e34e1b92e
MMDDKK6500 <woobiewhy@gmail.com>
2024-09-26 20:17:43 -0300

    Add NPCUtils, explain how to use lib, and add vscode settings

---
+++ BikeUtils/NPCUtils.cs
@@ -0,0 +1,46 @@
+﻿namespace BikeUtils;
+
+using Exiled.API.Features;
+using Exiled.API.Features.Components;
+using Mirror;
+using PlayerRoles;
+
+
+public class NPCUtils
+{
+    public static ReferenceHub SpawnDummy(RoleTypeId role, string name)
+    {
+
+        var newPlayer = UnityEngine.Object.Instantiate(NetworkManager.singleton.playerPrefab);
+
+        Npc npc = new(newPlayer)
+        {
+            IsNPC = true
+        };
+        
+        try
+        {
+            npc.ReferenceHub.roleManager.InitializeNewRole(role, RoleChangeReason.RemoteAdmin);
+        }
+        catch
+        { }
+
+        int num = UnityEngine.Random.Range(999, 99999);
+
+        var fakeConnection = new FakeConnection(num++);
+
+        var hubPlayer = newPlayer.GetComponent<ReferenceHub>();
+
+        NetworkServer.AddPlayerForConnection(fakeConnection, newPlayer);
+        try
+        {
+            npc.ReferenceHub.authManager.UserId = $"{num}@bogbuts";
+        }
+        catch
+        { }
+
+        hubPlayer.nicknameSync.Network_myNickSync = name;
+
+        return hubPlayer;
+        }
+}
--- README.md
+++ README.md
@@ -1,4 +1,18 @@
-# BikeUtils
+# ✨BikeUtils✨
 Uma lib de utilidades para plugins de EXILED, criado pro Bicicleta e atualizado por MMDDKK6500.
 
 É utilizado no servidor brasileiro Danger Containment
+
+# Como utilizar
+
+Crie o arquivo `BikeUtils.csproj.user` no mesmo local que `BikeUtils.csproj` e coloque isso dentro:
+```xml
+<Project>
+    <PropertyGroup>
+        <EXILED_REFERENCES>../include</EXILED_REFERENCES>
+    </PropertyGroup>
+</Project>
+```
+Mude o `../include` para a pasta onde guarda todas as assemblies do SCPSL, pode ser um caminho relativo ao arquivo, como o exemplo demonstra, ou pode ser um caminho absoluto como `C:\Program Files (x86)\Steam\steamapps\common\SCP Secret Laboratory Dedicated Server\SCPSL_Data\Managed` (Local padrão onde fica os arquivos do SCPSL Dedicated Server)
+
+No caso dessa lib, se utiliza as assemblies `Assembly-CSharp`, `Mirror` e `UnityEngine.CoreModule`, que já vem com o servidor dedicado.
