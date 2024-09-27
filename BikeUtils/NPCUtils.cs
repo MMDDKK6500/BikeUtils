4d7debe8867029685533232bd483a52dc7d87560
Edi <112036561+Edi369@users.noreply.github.com>
2024-09-26 23:47:41 -0300

    now yes, updating NpcDummy!!!! sorryrrrrr

--- BikeUtils/NPCUtils.cs
+++ BikeUtils/NPCUtils.cs
@@ -4,7 +4,8 @@ using Exiled.API.Features;
 using Exiled.API.Features.Components;
 using Mirror;
 using PlayerRoles;
-
+using System.Collections.Generic;
+using UnityEngine;
 
 public static class NPCUtils
 {
@@ -15,39 +16,39 @@ public static class NPCUtils
     /// <param name="role">The <see cref="RoleTypeId"/> that the dummy will have.</param>
     /// <param name="name">The name for the dummy.</param>
     /// <returns>Returns a <see cref="ReferenceHub"/> for the spawned dummy.</returns>
-    public static ReferenceHub SpawnDummy(RoleTypeId role, string name)
+    public static Dictionary<int, ReferenceHub> DummyList = new Dictionary<int, ReferenceHub>();
+
+    public static ReferenceHub? SpawnDummy(RoleTypeId role, string name, int id = 42)
     {
-
-        var newPlayer = UnityEngine.Object.Instantiate(NetworkManager.singleton.playerPrefab);
-
-        Npc npc = new(newPlayer)
+        if (!DummyList.ContainsKey(id))
         {
-            IsNPC = true
-        };
-        
-        try
-        {
-            npc.ReferenceHub.roleManager.InitializeNewRole(role, RoleChangeReason.RemoteAdmin);
+            GameObject newPlayer = Object.Instantiate(NetworkManager.singleton.playerPrefab);
+            Npc npc = new Npc(newPlayer) { IsNPC = true };
+            npc.ReferenceHub.roleManager.InitializeNewRole(RoleTypeId.None, RoleChangeReason.RemoteAdmin);
+            int num = Random.Range(999, 999999);
+            var fakeConnection = new FakeConnection(num++);
+            var hubPlayer = newPlayer.GetComponent<ReferenceHub>();
+            NetworkServer.AddPlayerForConnection(fakeConnection, newPlayer);
+            npc.ReferenceHub.authManager.UserId = $"{num}@basicdummy";
+            hubPlayer.nicknameSync.Network_myNickSync = name;
+            npc.ReferenceHub.roleManager.ServerSetRole(role, RoleChangeReason.RemoteAdmin);
+            DummyList.Add(id, hubPlayer);
+
+            return hubPlayer;
         }
-        catch
-        { }
+        Log.Warn($"Dummy with id {id} is ALREADY in use, use another id to create a new dummy!");
+        return null;
+    }
 
-        int num = UnityEngine.Random.Range(999, 99999);
+    public static ReferenceHub? GetDummy(int id) {
+        if (DummyList.ContainsKey(id))
+            return DummyList[id];
+        return null;
+    }
 
-        var fakeConnection = new FakeConnection(num++);
+    public static void RemoveDummy(int id) {
+        if (DummyList.ContainsKey(id))
+            NetworkServer.RemoveConnection(DummyList[id].PlayerId);
 
-        var hubPlayer = newPlayer.GetComponent<ReferenceHub>();
-
-        NetworkServer.AddPlayerForConnection(fakeConnection, newPlayer);
-        try
-        {
-            npc.ReferenceHub.authManager.UserId = $"{num}@bogbuts";
-        }
-        catch
-        { }
-
-        hubPlayer.nicknameSync.Network_myNickSync = name;
-
-        return hubPlayer;
-        }
+    }
 }
