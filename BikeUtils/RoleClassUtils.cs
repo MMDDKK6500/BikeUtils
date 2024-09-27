83b13466c56ef9f2fdead0e55b43457c3b0deadf
Edi <112036561+Edi369@users.noreply.github.com>
2024-09-27 13:40:34 -0300

    Modifying certain functions to be more compatible

--- BikeUtils/RoleClassUtils.cs
+++ BikeUtils/RoleClassUtils.cs
@@ -1,8 +1,8 @@
+namespace BikeUtils;
+
 using Exiled.API.Features;
 using PlayerRoles;
 using UnityEngine;
-
-namespace BikeUtils;
 
 public static class RoleClassUtils
 {
@@ -10,16 +10,14 @@ public static class RoleClassUtils
     /// Gets the player's tag color.
     /// </summary>
     /// <param name="player">The <see cref="Player"/> you want to get the color.</param>
-    /// <returns>Returns astring with the player color in a #000000 HEX fromat or #FFFFFF if not found.</returns>
+    /// <returns>Returns string with the player color in a #000000 HEX format or #FFFFFF if not found.</returns>
     public static string GetPlayerTagColor(Player player)
     {
-        var colorRoleServer = player.ReferenceHub.serverRoles.MyColor;
-        if (colorRoleServer == "default") colorRoleServer = "#FFFFFF";
-        return colorRoleServer;
+        return player.ReferenceHub.serverRoles.MyColor == "default" ? "#FFFFFF" : player.ReferenceHub.serverRoles.MyColor;
     }
 
     /// <summary>
-    /// Translates a <see cref="RoleTypeId"/> into the Brazillian Portuguese name of role.
+    /// Translates a <see cref="RoleTypeId"/> into the Brazilian Portuguese name of role.
     /// </summary>
     /// <param name="role">The <see cref="RoleTypeId"/> to be translated.</param>
     /// <returns>A string that contains the translated name, or ??? if role was not found.</returns>
@@ -52,11 +50,11 @@ public static class RoleClassUtils
     }
 
     /// <summary>
-    /// Gets a string and checks if it is a valid playable SCP number.
+    /// Gets a string or int and checks if it is a valid playable SCP number.
     /// </summary>
-    /// <param name="number">The SCP number, in a string format.</param>
+    /// <param name="number">The SCP number, in a string or int format.</param>
     /// <returns>Returns the <see cref="RoleTypeId"/> of the SCP, or Spectator if not an SCP.</returns>
-    public static RoleTypeId StringToSCPRole(string number)
+    public static RoleTypeId VarToSCPRole(string number)
     {
         return number switch
         {
@@ -70,6 +68,20 @@ public static class RoleClassUtils
             _ => RoleTypeId.Spectator,
         };
     }
+    public static RoleTypeId VarToSCPRole(int number)
+    {
+        return number switch
+        {
+            939 => RoleTypeId.Scp939,
+            106 => RoleTypeId.Scp106,
+            096 => RoleTypeId.Scp096,
+            173 => RoleTypeId.Scp173,
+            049 => RoleTypeId.Scp049,
+            079 => RoleTypeId.Scp079,
+            3114 => RoleTypeId.Scp3114,
+            _ => RoleTypeId.Spectator,
+        };
+    }
 
     /// <summary>
     /// Checks if the given <see cref="RoleTypeId"/> is an SCP Role.
@@ -78,13 +90,14 @@ public static class RoleClassUtils
     /// <returns>True if <see cref="RoleTypeId"/> is an SCP role or false if <see cref="RoleTypeId"/> is not an SCP role.</returns>
     public static bool ScpRoles(RoleTypeId role)
     {
-        return role == RoleTypeId.Scp049 ||
-               role == RoleTypeId.Scp0492 ||
-               role == RoleTypeId.Scp096 ||
+        return role == RoleTypeId.Scp173 ||
                role == RoleTypeId.Scp106 ||
-               role == RoleTypeId.Scp173 ||
-               role == RoleTypeId.Scp3114 ||
-               role == RoleTypeId.Scp939;
+               role == RoleTypeId.Scp049 ||
+               role == RoleTypeId.Scp079 ||
+               role == RoleTypeId.Scp096 ||
+               role == RoleTypeId.Scp0492 ||
+               role == RoleTypeId.Scp939 ||
+               role == RoleTypeId.Scp3114;
     }
 
     /// <summary>
@@ -120,8 +133,19 @@ public static class RoleClassUtils
     /// <param name="boostG">Green color boost.</param>
     /// <param name="boostB">Blue color boost.</param>
     /// <returns>A string with the HEX color in the #000000 format.</returns>
-    public static string UnityColorToHex(Color color, float boostR = 0f, float boostG = 0f, float boostB = 0f)
+    public static string UnityColorToHex(Color color, float boostR, float boostG, float boostB)
     {
         return $"#{Mathf.FloorToInt(Mathf.Clamp01(color.r+boostR)*255):X2}{Mathf.FloorToInt(Mathf.Clamp01(color.g+boostG)*255):X2}{Mathf.FloorToInt(Mathf.Clamp01(color.b+boostB)*255):X2}";
+    }
+
+    /// <summary>
+    /// Same thing as the other function, but now the boost variable increases all color channels
+    /// </summary>
+    /// <param name="color"><see cref="Color">Unity Color</see> object.</param>
+    /// <param name="boost">All channels color boost.</param>
+    /// <returns>A string with the HEX color in the #000000 format.</returns>
+    public static string UnityColorToHex(Color color, float boost = 0f)
+    {
+        return $"#{Mathf.FloorToInt(Mathf.Clamp01(color.r + boost) * 255):X2}{Mathf.FloorToInt(Mathf.Clamp01(color.g + boost) * 255):X2}{Mathf.FloorToInt(Mathf.Clamp01(color.b + boost) * 255):X2}";
     }
 }
