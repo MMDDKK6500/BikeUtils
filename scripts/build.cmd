37ae6e2c68b77feb2890a9dec63c253211e603a6
MMDDKK6500 <woobiewhy@gmail.com>
2024-09-26 21:26:54 -0300

    Fix build script and add info to csproj file

--- scripts/build.cmd
+++ scripts/build.cmd
@@ -7,9 +7,8 @@ REM call scripts/check.cmd
 dotnet clean
 dotnet build
 if not exist bin mkdir bin
-copy Adrenaline\bin\Debug\net48\Adrenaline.dll bin\Adrenaline.dll
-copy Push\bin\Debug\net48\Push.dll bin\Push.dll
-echo Copied all plugin DLLs into bin folder!
+copy BikeUtils\bin\Debug\net48\BikeUtils.dll bin\BikeUtils.dll
+echo Copied all compiled DLLs into bin folder!
 goto end
 
 :invalid
