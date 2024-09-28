25fc0182fe9c48caf0c1e8d22b53200c4a70bd28
Edi <112036561+Edi369@users.noreply.github.com>
2024-09-28 00:40:24 -0300

    STRINGUTILS!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

---
+++ BikeUtils/StringUtils.cs
@@ -0,0 +1,17 @@
+﻿namespace BikeUtils;
+
+public static class StringUtils
+{
+    /// <summary>
+    /// Generate Automatically generates line breaks for lazy peoples.
+    /// </summary>
+    /// <param name="quantity">It's the number of line breaks it will generate.</param>
+    /// <returns>Returns a string with the number of line breaks supplied.</returns>
+    public static string GenerateLineBreak(int quantity) {
+        string linebreak = "";
+        for (int i = 0; i < quantity; i++) {
+            linebreak += "\n";
+        }
+        return linebreak;
+    }
+}
