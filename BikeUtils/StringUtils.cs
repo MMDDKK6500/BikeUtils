42e9b15c6e907a153d4a9b21ee0bc7d73636d407
Edi <112036561+Edi369@users.noreply.github.com>
2024-09-29 16:53:49 -0300

    HERE YOUR DOCUMENTATION!!!!!!!!!!!!!!!!

--- BikeUtils/StringUtils.cs
+++ BikeUtils/StringUtils.cs
@@ -8,19 +8,43 @@ using System.Text;
 /// </summary>
 public static class StringUtils
 {
+    /// <summary>
+    /// A enum containing the types of modifications that can be applied to a string.
+    /// </summary>
     public enum StrModiType
     {
+        /// <summary>
+        /// Type to align the string.
+        /// </summary>
         Align,
+        /// <summary>
+        /// Type to change the string's position.
+        /// </summary>
         Pos,
+        /// <summary>
+        /// Type to change the vertical offset of the string.
+        /// </summary>
         Voffset,
+        /// <summary>
+        /// Type to change the position and voffset.
+        /// </summary>
         PosVoffset,
+        /// <summary>
+        /// Type to change position and align the text.
+        /// </summary>
         PosAlign,
+        /// <summary>
+        /// Type to change the voffset and alignment.
+        /// </summary>
         VoffsetAlign,
+        /// <summary>
+        /// Type to change all modifiers.
+        /// </summary>
         PosVoffsetAlign
     }
 
     /// <summary>
-    /// Generate Automatically generates line breaks for lazy peoples.
+    /// Generate Automatically generates line breaks for lazy people.
     /// </summary>
     /// <param name="quantity">The number of line breaks it will generate.</param>
     /// <returns>Returns a string with the number of line breaks supplied.</returns>
@@ -34,6 +58,13 @@ public static class StringUtils
         return linebreak;
     }
 
+    /// <summary>
+    /// Makes it easy to modify text with multiple lines.
+    /// </summary>
+    /// <param name="text">It's the text that's going to be changed.</param>
+    /// <param name="modify">The type of change that will be made to the text. Using <see cref="StrModiType"/> to choose the type of modification.</param>
+    /// <param name="quant">Defines the amount of change to be made to the text.</param>
+    /// <returns>Returns the text with the changes applied.</returns>
     public static string AdjustString(string text, StrModiType modify, int quant)
     {
         StringBuilder result = new StringBuilder();
@@ -60,6 +91,15 @@ public static class StringUtils
         return result.ToString();
     }
 
+    /// <summary>
+    /// Makes it easy to modify text with multiple lines.
+    /// </summary>
+    /// <param name="text">It's the text that's going to be changed.</param>
+    /// <param name="modify">The type of change that will be made to the text. Using <see cref="StrModiType"/> to choose the type of modification.</param>
+    /// <param name="quant1">Defines the quantity that will be applied to the Pos.</param>
+    /// <param name="quant2">Defines the amount that will be applied to Vofsset.</param>
+    /// <param name="direction">Defines the direction of the alignment.</param>
+    /// <returns>Returns the text with the changes applied.</returns>
     public static string AdjustString(string text, StrModiType modify, int quant1, int quant2, string direction = "")
     {
         StringBuilder result = new StringBuilder();
@@ -85,6 +125,15 @@ public static class StringUtils
 
         return result.ToString();
     }
+
+    /// <summary>
+    /// Makes it easy to modify text with multiple lines.
+    /// </summary>
+    /// <param name="text">It's the text that's going to be changed.</param>
+    /// <param name="modify">The type of change that will be made to the text. Using <see cref="StrModiType"/> to choose the type of modification.</param>
+    /// <param name="quant">Defines the amount of change to be made to the text.</param>
+    /// <param name="direction">Defines the direction of the alignment.</param>
+    /// <returns>Returns the text with the changes applied.</returns>
     public static string AdjustString(string text, StrModiType modify, int quant, string direction)
     {
         StringBuilder result = new StringBuilder();
@@ -110,6 +159,14 @@ public static class StringUtils
 
         return result.ToString();
     }
+
+    /// <summary>
+    /// Makes it easy to modify text with multiple lines.
+    /// </summary>
+    /// <param name="text">It's the text that's going to be changed.</param>
+    /// <param name="modify">The type of change that will be made to the text. Using <see cref="StrModiType"/> to choose the type of modification.</param>
+    /// <param name="direction">Defines the direction of the alignment.</param>
+    /// <returns>Returns the text with the changes applied.</returns>
     public static string AdjustString(string text, StrModiType modify, string direction)
     {
         StringBuilder result = new StringBuilder();
